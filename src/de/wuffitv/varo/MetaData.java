f1a1d51aba655e399dee619e03a1e653ba12de72
Wuffi2405 <wuffi2405@gmail.com>
2017-01-21 18:38:11 +0100

    update -add{MetaData}

---
+++ src/de/wuffitv/varo/MetaData.java
@@ -0,0 +1,11 @@
+package de.wuffitv.varo;
+
+import java.util.ArrayList;
+
+import org.bukkit.entity.Player;
+
+public class MetaData {
+	
+	public ArrayList<Player> players = new ArrayList<>();
+	
+}
