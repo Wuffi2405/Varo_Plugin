2f8b2698f6a70f5d71e6d648ab54a6b54c206247
Wuffi2405 <wuffi2405@gmail.com>
2017-01-21 12:45:21 +0100

    A10

---
+++ src/de/wuffitv/varo/event/VARO_PlayerDeathEvent.java
@@ -0,0 +1,24 @@
+package de.wuffitv.varo.event;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.plugin.Plugin;
+
+public class VARO_PlayerDeathEvent implements Listener {
+	
+	public static Plugin plugin;
+	
+	@SuppressWarnings("static-access")
+	public VARO_PlayerDeathEvent(Plugin plugin){
+		this.plugin = plugin;
+	}
+	
+	@EventHandler
+	public void onPlayerDeath(PlayerDeathEvent event){
+		Player player = (Player)event.getEntity().getPlayer();
+		player.kickPlayer("Du bist gestorben");
+	}
+	
+}
