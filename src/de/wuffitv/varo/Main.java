fd38fbe8e45440d495f27a226aa24818db8bd200
Wuffi2405 <wuffi2405@gmail.com>
2017-01-21 12:19:00 +0100

    A9

--- src/de/wuffitv/varo/Main.java
+++ src/de/wuffitv/varo/Main.java
@@ -1,5 +1,6 @@
 package de.wuffitv.varo;
 
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin{
@@ -7,13 +8,13 @@ public class Main extends JavaPlugin{
 	@Override
 	public void onEnable() {
 		super.onEnable();
-		
+		Bukkit.getConsoleSender().sendMessage("[VARO] was enabled");
 	}
 	
 	@Override
 	public void onDisable() {
 		super.onDisable();
-		
+		Bukkit.getConsoleSender().sendMessage("[VARO] was disabled");
 	}
 	
 }
