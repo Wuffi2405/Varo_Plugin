a52af3cf075254561bc904e5c797898d3484003c
Wuffi2405 <wuffi2405@gmail.com>
2017-01-21 15:43:51 +0100

    A12

--- src/de/wuffitv/varo/event/VARO_PlayerDeathEvent.java
+++ src/de/wuffitv/varo/event/VARO_PlayerDeathEvent.java
@@ -18,9 +18,10 @@ public class VARO_PlayerDeathEvent implements Listener {
 	@EventHandler
 	public void onPlayerDeath(PlayerDeathEvent event){
 		
-		String killer = (String)event.getEntity().getKiller().getName();
+//		String killer = (String)event.getEntity().getKiller().getName();
 		Player player = (Player)event.getEntity().getPlayer();
-		player.kickPlayer("Du bist gestorben und getötet durch " + killer);
+//		player.kickPlayer("Du bist gestorben und getötet durch " + killer);
+		player.setBanned(true);
 	}
 	
 }
