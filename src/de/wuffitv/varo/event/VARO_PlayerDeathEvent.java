cfb8598001edd537864f55206375145c7fa74543
Wuffi2405 <wuffi2405@gmail.com>
2017-01-21 16:41:13 +0100

    A22

--- src/de/wuffitv/varo/event/VARO_PlayerDeathEvent.java
+++ src/de/wuffitv/varo/event/VARO_PlayerDeathEvent.java
@@ -3,6 +3,7 @@ package de.wuffitv.varo.event;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.plugin.Plugin;
@@ -19,12 +20,12 @@ public class VARO_PlayerDeathEvent implements Listener {
 	}
 
 	@SuppressWarnings("deprecation")
-	@EventHandler
+	@EventHandler(priority = EventPriority.HIGH)
 	public void onPlayerDeath(PlayerDeathEvent event) {
 
 		String killer = (String) event.getEntity().getKiller().getName();
 		Player player = (Player) event.getEntity().getPlayer();
-		player.setHealth(10);
+		player.setHealth(20);
 		player.kickPlayer("Du bist gestorben und getötet durch " + killer);
 		player.setBanned(true);
 
