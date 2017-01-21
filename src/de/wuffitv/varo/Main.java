56410c0416c50bce661966cf6ebc883135378b76
Wuffi2405 <wuffi2405@gmail.com>
2017-01-21 12:27:23 +0100

    A9

--- src/de/wuffitv/varo/Main.java
+++ src/de/wuffitv/varo/Main.java
@@ -1,6 +1,10 @@
 package de.wuffitv.varo;
 
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin{
@@ -17,4 +21,15 @@ public class Main extends JavaPlugin{
 		Bukkit.getConsoleSender().sendMessage("[VARO] was disabled");
 	}
 	
+	@Override
+	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+		if(sender instanceof Player){
+			Player player = (Player)sender;
+			if(label.equalsIgnoreCase("google")){
+				player.sendMessage(ChatColor.BLUE + "G" + ChatColor.RED + "o" + ChatColor.YELLOW + "o" + ChatColor.BLUE + "g" + ChatColor.GREEN + "l" + ChatColor.RED + "e" + ChatColor.WHITE);
+				return true;
+			}
+		}
+		return super.onCommand(sender, command, label, args);
+	}
 }
