41619f9bdab8dd210cc11b9176483457a551ab89
Wuffi2405 <wuffi2405@gmail.com>
2017-01-21 18:21:41 +0100

    update add{more comments}

--- src/de/wuffitv/varo/event/VARO_PlayerJoinEvent.java
+++ src/de/wuffitv/varo/event/VARO_PlayerJoinEvent.java
@@ -5,13 +5,24 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 
+import net.md_5.bungee.api.ChatColor;
+
 public class VARO_PlayerJoinEvent implements Listener{
 
-	
+	/**
+	 * Wenn der Spieler joint
+	 */
 	@EventHandler
 	public void onJoin(PlayerJoinEvent e) {
-		Player p = e.getPlayer();
-		e.setJoinMessage(p.getDisplayName() + " has joined the game");
+		/**
+		 * init Variables
+		 */
+		Player player = e.getPlayer();
+		
+		/**
+		 * create Join Message
+		 */
+		e.setJoinMessage(ChatColor.RED + player.getDisplayName() + ChatColor.WHITE + " has joined the game");
 
 	}
 	
