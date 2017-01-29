035094d01617844092400607c2c7358096c9e434
Wuffi2405 <wuffi2405@gmail.com>
2017-01-29 18:54:46 +0100

    update -version{0.1.0[Alpha1.0]}

--- src/de/wuffitv/team/Teams.java
+++ src/de/wuffitv/team/Teams.java
@@ -1,17 +1,10 @@
 package de.wuffitv.team;
 
-import java.util.ArrayList;
-
 public class Teams {
-	
-	public static ArrayList<String> team_long = new ArrayList<>();
-	public static ArrayList<String> team_short = new ArrayList<>();
-	public static ArrayList<String> team_member1 = new ArrayList<>();
-	public static ArrayList<String> team_member2 = new ArrayList<>();
 	
 	public Teams(){
 		
-		//---
+		
 		
 	}
 	
--- src/de/wuffitv/varo/util/Config.java
+++ src/de/wuffitv/varo/util/Config.java
@@ -28,7 +28,9 @@ public class Config {
 			fileConfiguration.set(".border_size", 500);
 			fileConfiguration.set(".border_size_min", 100);
 			fileConfiguration.set(".border_size_lower_time", 20);
-		
+			
+			fileConfiguration.set(".teams_value", 5);
+			//---
 			fileConfiguration.set(".team1-long", 0);
 			fileConfiguration.set(".team1-short", 0);
 			fileConfiguration.set(".team1-member1", 0);
@@ -79,6 +81,12 @@ public class Config {
 			fileConfiguration.set(".team10-member1", 0);
 			fileConfiguration.set(".team10-mebmer2", 0);
 			
+			/**
+			 * Hier die Klasse nochmal aufrufen, weil sonst die Spawn Koords nicht funktionieren.
+			 * Gilt nur für erst rl
+			 */
+			loadData();
+			
 			try {
 				fileConfiguration.save(configFile);
 			} catch (IOException e) {
@@ -89,17 +97,14 @@ public class Config {
 			Bukkit.getConsoleSender().sendMessage(ChatColor.RED + "ERROR");
 		}
 		
-		/**
-		 * Hier die Klasse nochmal aufrufen, weil sonst die Spawn Koords nicht funktionieren.
-		 * Gilt nur für erst rl
-		 */
-		loadData();
 	}
 	
 	void loadData(){
 		/**
 		 * laden
 		 */
+		MetaData.teams_value = fileConfiguration.getInt(".teams_value");
+		
 		MetaData.spawn_x = fileConfiguration.getInt(".spawn_x");
 		MetaData.spawn_z = fileConfiguration.getInt(".spawn_z");
 		MetaData.spawn_y = fileConfiguration.getInt(".spawn_y");
@@ -108,8 +113,21 @@ public class Config {
 		MetaData.border_size_min = fileConfiguration.getInt(".border_size_min");
 		MetaData.border_size_lower_time = fileConfiguration.getInt(".border_size_lower_time");
 		//---
-		MetaData.team1_long = fileConfiguration.getString(".team1-long");
 		
+		for(int i = 0; MetaData.teams_value > i; i++){
+			String s_long = ".team"+(i+1)+"-long";
+			MetaData.teams_names_long.add(i, fileConfiguration.getString(s_long));
+			
+			String s_short = ".team"+(i+1)+"-short";
+			MetaData.teams_names_short.add(i, fileConfiguration.getString(s_short));
+			
+			String s_member1 = ".team"+(i+1)+"-member1";
+			MetaData.teams_names_member1.add(i, fileConfiguration.getString(s_member1));
+			
+			String s_member2 = ".team"+(i+1)+"-member2";
+			MetaData.teams_names_member2.add(i, fileConfiguration.getString(s_member2));
+			
+		}
 		
 		/**
 		 * Debugging Ausgabe
@@ -118,6 +136,15 @@ public class Config {
 			Bukkit.getConsoleSender().sendMessage(ChatColor.RED + "X:" + MetaData.spawn_x);
 			Bukkit.getConsoleSender().sendMessage(ChatColor.RED + "Z:" + MetaData.spawn_z);
 			Bukkit.getConsoleSender().sendMessage(ChatColor.RED + "Y:" + MetaData.spawn_y);
+			
+			System.out.println("Long: " + MetaData.teams_names_long.size());
+			System.out.println("Short: " + MetaData.teams_names_short.size());
+			System.out.println("Member1: " + MetaData.teams_names_member1.size());
+			System.out.println("Member2: " + MetaData.teams_names_member2.size());
+			
+			for(int i = 0; i < MetaData.teams_names_long.size(); i++){
+				System.out.println(""+MetaData.teams_names_long.get(i));
+			}
 		}
 		
 	}
